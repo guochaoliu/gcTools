f1dfbba8cd5caf5f8671f94462f9f65ed61bcb3e
david-health <guo_chao_liu@sina.com>
2024-07-28 20:32:08 +0800

    新建测试Module

---
+++ gcTools/src/main/java/com/guochao/gctools/GCToolsTest.java
@@ -0,0 +1,15 @@
+package com.guochao.gctools;
+
+import android.content.Context;
+import android.widget.Toast;
+
+/**
+ * Created by guo_chao_liu on 2024/7/28.
+ * description
+ */
+public class GCToolsTest {
+
+    public static void test(Context context){
+        Toast.makeText(context, "测试", Toast.LENGTH_SHORT).show();
+    }
+}
